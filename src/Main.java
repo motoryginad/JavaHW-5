5f59ae4387c80988c6a67eb860dab37f14410e57
Andrey Motorygin <motoryginad@gmail.com>
2020-09-03 12:35:20 +0300

    CodeToBonusMiles

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        float ticketPrice = 490;
+        int freeMiles = (int) ticketPrice/20;
+        System.out.println(freeMiles);
+    }
+}
