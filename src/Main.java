4c4cb6c1fae93734d91e95768de1c8953d1e3915
Andrey Motorygin <motoryginad@gmail.com>
2020-09-07 12:21:29 +0300

    CtrlAltL

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         int ticketPrice = 8654;
-        int freeMiles = ticketPrice/20;
+        int freeMiles = ticketPrice / 20;
         System.out.println(freeMiles);
     }
 }
